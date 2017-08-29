3b317981e95ec68554940dcd1aa429ad67e8afe2
vmerugu1 <31452964+vmerugu1@users.noreply.github.com>
2017-08-29 22:45:51 +0100

    Create Runner.java

---
+++ Runner.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\Venkat\\workspace\\QAWorks\\src\\test\\resources\\Contact.feature")
+public class Runner {
+
+}
